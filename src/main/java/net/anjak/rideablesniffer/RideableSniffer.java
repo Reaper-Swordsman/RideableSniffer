ee99c068614eaaa938ea689b59507b22116acf97
Reaper-Swordsman <86449542+Reaper-Swordsman@users.noreply.github.com>
2023-07-09 13:43:01 -0500

    Initial commit

---
+++ src/main/java/net/anjak/rideablesniffer/RideableSniffer.java
@@ -0,0 +1,15 @@
+package net.anjak.rideablesniffer;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RideableSniffer implements ModInitializer {
+	public static final java.lang.String MOD_ID = "rideablesniffer";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/anjak/rideablesniffer/RideableSnifferClient.java
@@ -0,0 +1,10 @@
+package net.anjak.rideablesniffer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RideableSnifferClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
